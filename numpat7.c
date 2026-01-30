facae1484a7278173df402894ddef2a8a9154796
Arpita Sarode <arpitasarode1@gmail.com>
2026-01-30 13:30:08 +0530

    Add files via upload

---
+++ numpat7.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int n=3;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n;j++){
+            if((i+j)%2==0){
+                printf("0 ");
+            }
+            else{
+                printf("1 ");
+            }
+
+        }printf("\n");
+    }
+}

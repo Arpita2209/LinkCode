3394e3516cdd5e2b70a7af271e88fc4e2ce19c22
Arpita Sarode <arpitasarode1@gmail.com>
2026-02-07 11:27:12 +0530

    Add files via upload

---
+++ array2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int a[3]={10,20,30};
+    printf("%d\n",a[2]);
+    //update
+    // a[2]=200;
+    // printf("%d",a[2]);
+
+    for(int i=0;i<3;i++){
+        printf("%d\n",a[i]);
+    }
+
+
+
+}

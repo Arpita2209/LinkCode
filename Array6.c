63c444f99c9e0da8a4438afd70db0a4362b2fe8b
Arpita Sarode <arpitasarode1@gmail.com>
2026-02-09 18:33:19 +0530

    Add files via upload

---
+++ Array6.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int max=arr[0];
+    int arr[]={23,34,65,50,21,4}
+
+    for(int i=0;i<=6,i++){
+        if(arr[i]>max){
+            max=arr[i];
+        }
+    }
+    printf("%d",max);
+}

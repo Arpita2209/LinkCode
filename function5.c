fff89ab96a7e0818c695df37fda32a9ee63df778
Arpita Sarode <arpitasarode1@gmail.com>
2026-02-24 19:22:45 +0530

    Add files via upload

---
+++ function5.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+
+// circlearea();
+// trianglearea();
+  void circlearea(){
+        float radius,area;
+        printf("enter radius:");
+        scanf("%f",&radius);
+        area=3.14*radius*radius;
+        printf("%f",area);
+
+    }
+
+     void trianglearea(){
+        float base,height,area;
+        printf("enter base height:");
+        scanf("%f%f",&base,&height);
+        area=0.5*base*height;
+        printf("%f",area);
+
+    }
+
+
+int main(){
+    int ch,ct;
+     float base,height;
+     float radius;
+ 
+
+    printf("\n1-area of circle\n2-area of triangle\n");
+
+    printf("\nenter your choice:");
+    scanf(" %d",&ch);
+    while(1){
+    switch(ch)
+    {
+    case 1:
+          printf("enter radius:");
+        scanf("%f",&radius);
+        circlearea(radius);
+
+        break;
+    case 2:
+        printf("enter base height:");
+        scanf("%f%f",&base,&height);
+        trianglearea(float base,float height);
+
+        break;
+    }
+    printf("if you want to continue press 1");
+    scanf("%d",&ct);
+    if(ct!=1){
+        break;
+    }
+}
+
+
+}
+
+  
